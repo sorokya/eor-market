6d336079d74ae5fb5e5426ee86fc3b15739460f3
Richard Leek <richard@richardleek.com>
2024-11-11 02:29:16 +0100

    Add planned features

--- src/about.php
+++ src/about.php
@@ -17,13 +17,26 @@ $user = isset($_COOKIE['user']) ? json_decode($_COOKIE['user']) : null;
 <body>
   <? include './header.php'; ?>
 
-  <div id="about">
+  <div class="about">
     <p style="text-align: center">EO Market is a peer to peer marketplace for Endless Online. It is free to use and has
       no
       ads.</p>
 
     <p style="text-align: center">If you would like to support the project, you can buy me a Ko-fi <a
         href="https://ko-fi.com/sorokya" target="_blank">here</a>.</p>
+  </div>
+
+  <br />
+
+  <div class="about">
+    <h3>Planned features:</h3>
+    <ul style="display: table; margin: auto; text-align:left;">
+      <li>Show online status of players</li>
+      <li>Add buy offers in addition to sell offers</li>
+      <li>View recent verified trades</li>
+      <li>View price data for all traded items</li>
+      <li>Leaderboard (player with most trades)</li>
+    </ul>
   </div>
 
   <? include './footer.php'; ?>
